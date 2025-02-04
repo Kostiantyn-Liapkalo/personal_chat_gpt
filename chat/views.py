9b4a5a1d5d45d00631331c52bd6177e12dc103f1
Kostiantyn Liapkalo <lynx87@ukr.net>
2025-02-04 23:17:05 +0200

    first commit

---
+++ chat/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import ChatMessage
+from .services import get_chatgpt_response
+from django.contrib.auth.forms import UserCreationForm
+
+@login_required
+def chat_view(request):
+    if request.method == 'POST':
+        user_message = request.POST.get('message')
+        response = get_chatgpt_response(user_message)
+        ChatMessage.objects.create(user=request.user, message=user_message, response=response)
+        return render(request, 'chat/chat.html', {'response': response})
+
+    return render(request, 'chat/chat.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
